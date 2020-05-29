8a3fd39fc3bb2cea17ba449b168f76686adf2c99
arohlen <arohlen@kth.se>
2020-05-29 16:55:46 +0200

    Updates

---
+++ precache-manifest.3d01e7a3d11b681fc3c33934de31d522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdde9a46c9debfc3b5058b4733a9804",
+    "url": "/digitclassifier/index.html"
+  },
+  {
+    "revision": "6fcfcc991c347d0308cf",
+    "url": "/digitclassifier/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "adeef9b5a2bb9366dd9c",
+    "url": "/digitclassifier/static/css/main.4e03e814.chunk.css"
+  },
+  {
+    "revision": "6fcfcc991c347d0308cf",
+    "url": "/digitclassifier/static/js/2.c0af1b3c.chunk.js"
+  },
+  {
+    "revision": "03d8dcb4c6506bee8b0de1d30ba21dfc",
+    "url": "/digitclassifier/static/js/2.c0af1b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adeef9b5a2bb9366dd9c",
+    "url": "/digitclassifier/static/js/main.fff2c61d.chunk.js"
+  },
+  {
+    "revision": "71a0941054054dd71f3b",
+    "url": "/digitclassifier/static/js/runtime-main.7ac0f05c.js"
+  }
+]);
